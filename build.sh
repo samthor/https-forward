9d804d7c36d68c0b9f026a4e52d73316e6bd537c
Sam Thorogood <sam.thorogood@gmail.com>
2019-07-04 15:55:29 +1000

    remove stuff replaced by snap

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-go build -o https-forward *.go
-
--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo touch /etc/https-forward
-sudo systemctl enable $(realpath https-forward.service)
-sudo mkdir -p /tmp/autocert
-sudo chown nobody:users /tmp/autocert
-sudo chmod 750 /tmp/autocert
